a9893c106a9928fccc17c04dcf3d433a1f12c801
Alex <omtmn@icloud.com>
2021-08-30 15:20:42 -0400

    get request by ID

--- api/server.js
+++ api/server.js
@@ -16,4 +16,31 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+server.post('/api/users', (req, res) => {
+    const newUser = req.body // info about the new user is in the requests body 
+    data.insert(newUser)
+    .then((users) => {
+        res.status(201).json(users)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: 'provide name and bio' })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    data.findById(req.params.id)
+    .then((user) => {
+        if(user){
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ message: `${req.params.id} does not exist` })
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: `${req.params.id} does not exist`})
+    })
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
