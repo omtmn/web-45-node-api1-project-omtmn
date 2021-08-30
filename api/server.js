37968fcbad8b6d1fb7707891d9e066f22036499e
Alex <omtmn@icloud.com>
2021-08-30 15:01:41 -0400

    get request + officially set up nodemon

--- api/server.js
+++ api/server.js
@@ -1,3 +1,19 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const data = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+    data.find()
+    .then((user) => {
+        res.status(200).json(user)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: 'users do not exist' })
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const server = require('./api/server');
 
-const port = 5000;
+const port = 3000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, (req, res) => {
+    console.log('Listening on port 3000!')
+})
